a70a4d46ef678db43b3b239039f2273fb0fe485a
ShenyuSun <shenyu0930@gmail.com>
2016-07-31 16:14:44 -0400

    Adding more

--- MySQL_Basic_Query.sql
+++ MySQL_Basic_Query.sql
@@ -1,5 +1,47 @@
+-- Retrieve static value --
+
 SELECT 'MyFirstValue' AS SomeValue;
 
 SELECT 1+1 AS TWO;
 
-SELECT NOW( AS NOW);
+SELECT NOW() AS NOW;
+SELECT CURDATE();
+SELECT CURTIME();
+
+SELECT PI();
+
+SELECT MOD(45, 7);
+
+SELECT SQRT(25);
+
+-- Retrieve all the data from table --
+
+SELECT *
+FROM sakila.actor;
+
+SELECT *
+FROM sakila.city;
+
+USE sakila;
+SELECT *
+FROM city;
+
+-- Retrieve all the data ordered by single column
+
+SELECT * 
+FROM actor 
+ORDER BY first_name;
+
+SELECT *
+FROM actor
+ORDER BY last_name;
+
+SELECT *
+FROM actor
+ORDER BY first_name DESC;
+
+SELECT *
+FROM actor
+ORDER BY last_name DESC;
+
+
